b55824afca618afb25d802dc30deb55eb37feb65
Kar-Sha <76408122+Kar-Sha@users.noreply.github.com>
2025-03-02 22:49:00 -0800

    Update mastodon_test.py

--- backend/mastodon_test.py
+++ backend/mastodon_test.py
@@ -1,5 +1,6 @@
-from mastodon_client import MastodonClient 
+from mastodon_client import MastodonClient
 import pytest
+from unittest.mock import patch
 
 client = MastodonClient()
 client.register_app('cs272app', 'https://mastodon.social')
@@ -7,12 +8,32 @@ client.authenticate_client()
 client.log_in(input("Enter the OAuth authorization code: "))
 client.initialize_user()
 
-def test_posting():
-    assert client.create_post("test") != None
+#tests to creates a post with a unique id
+@patch("mastodon_client.MastodonClient.create_post")
+def test_posting(mock_create_post):
+    mock_create_post.return_value = {"id": 11111, "content": "test"}
+    assert client.create_post("test")["id"] == 11111
 
-def test_retrieving(self,id):
-    assert client.get_post(id) != None
+#test to get the post from the unique id
+@patch("mastodon_client.MastodonClient.get_post")
+def test_retrieving(mock_get_post):
+    mock_get_post.return_value = {"id": 11111, "content": "test"}
+    assert client.get_post(11111)["id"] == 11111
 
+#test to see if the post gets deleted
+@patch("mastodon_client.MastodonClient.delete_post")
+def test_deleting(mock_delete_post):
+    mock_delete_post.return_value = True
+    assert client.delete_post(11111) is True
 
-def test_deleting(id):
-    assert client.create_post(id) != None
+#test invalid retrieval
+@patch("mastodon_client.MastodonClient.get_post")
+def test_retrieving_invalid_post(mock_get_post):
+    mock_get_post.return_value = None
+    assert client.get_post(99999) is None
+
+#test empty post wont be allowed
+@patch("mastodon_client.MastodonClient.create_post")
+def test_create_empty_post(mock_create_post):
+    mock_create_post.return_value = None
+    assert client.create_post("") is None
