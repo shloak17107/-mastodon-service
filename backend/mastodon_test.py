1269f014de6dc5ef2c498192c7789809d08d0f65
Kar-Sha <76408122+Kar-Sha@users.noreply.github.com>
2025-03-02 23:27:09 -0800

    Update mastodon_test.py

--- backend/mastodon_test.py
+++ backend/mastodon_test.py
@@ -2,6 +2,19 @@ from mastodon_client import MastodonClient
 import pytest
 from unittest.mock import patch
 
+failed_client = MastodonClient()
+def test_posting_error():
+    with pytest.raises(ValueError):
+        failed_client.create_post("test")
+
+def test_retrieve_error():
+    with pytest.raises(ValueError):
+        failed_client.get_post("test")
+
+def test_delete_error():
+    with pytest.raises(ValueError):
+        failed_client.delete_post("test")
+
 client = MastodonClient()
 client.register_app('cs272app', 'https://mastodon.social')
 client.authenticate_client()
